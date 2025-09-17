8bafb0e87400e4128a161479eae73c1560fc8fb2
MarioNaise <102912402+MarioNaise@users.noreply.github.com>
2025-09-17 18:13:16 +0200

    Improve string evaluation

--- cmd/lox/interpreter.go
+++ cmd/lox/interpreter.go
@@ -2,7 +2,6 @@ package lox
 
 import (
 	"fmt"
-	"reflect"
 	"strconv"
 	"strings"
 )
@@ -187,8 +186,8 @@ func (i *interpreter) visitComparison(e *expressionComparison) any {
 func (i *interpreter) visitTerm(e *expressionTerm) any {
 	switch e.tokenType() {
 	case Plus:
-		if ok, left, right := i.evaluatesToString(e); ok {
-			return fmt.Sprintf("%v%v", left, right)
+		if strVal, ok := i.evaluateToString(e); ok {
+			return strVal
 		}
 		left := i.parseFloat(e.expr())
 		right := i.parseFloat(e.next())
@@ -337,17 +336,11 @@ func (i *interpreter) visitExpr(e *exp) any {
 	return ""
 }
 
-func (i *interpreter) evaluatesToString(e expression) (bool, any, any) {
+func (i *interpreter) evaluateToString(e expression) (string, bool) {
 	left := i.evaluate(e.expr())
 	right := i.evaluate(e.next())
-	if left == nil {
-		left = "nil"
-	}
-	if right == nil {
-		right = "nil"
-	}
-	return reflect.TypeOf(left).Name() == "string" &&
-		reflect.TypeOf(right).Name() == "string", left, right
+	return fmt.Sprintf("%v%v", left, right), fmt.Sprintf("%T", left) == "string" ||
+		fmt.Sprintf("%T", right) == "string"
 }
 
 func (i *interpreter) parseFloat(e expression) float64 {
